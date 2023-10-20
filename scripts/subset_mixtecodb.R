b25bdc9cc9c74f03288bf6902a683df5bccc7826
Sandra Auderset <sandrauderset@gmail.com>
2023-10-20 14:41:40 +0200

    release update

--- scripts/subset_mixtecodb.R
+++ scripts/subset_mixtecodb.R
@@ -1,34 +1,33 @@
-# this script subsets and extracts the Mixtec data from the Mixtecan Cognate Database
+# this script subsets and extracts the Mixtec data from the Mixtecan Cognate Database v1.1
 
 library(pacman)
-devtools::install_github("ropensci/bib2df")
-p_load(bib2df,
-       here,
-       RefManageR,
+p_load(here,
+       rbibutils,
        tidyverse)
 
 
-# read in metadata, cognate sets, and sources
-options(encoding="UTF-8")
-sources <- bib2df("https://raw.githubusercontent.com/SAuderset/MixteCoDB/main/sources.bib")
-
-metadata <- read_tsv("https://raw.githubusercontent.com/SAuderset/MixteCoDB/main/metadata.tsv")
-cognates <- read_tsv("https://raw.githubusercontent.com/SAuderset/MixteCoDB/main/mixtecan_cognate_db.tsv")
+# read in metadata, cognate sets, protoforms
+metadata <- read_tsv(here("data/MixteCoDB-1.1/metadata.tsv"))
+cognates <- read_tsv(here("data/MixteCoDB-1.1/mixtecan_cognate_db.tsv"))
+protoforms <- read_tsv(here("data/MixteCoDB-1.1/protoforms.tsv"))
 
 # create vector of languages coded for sound changes
 segment_sample <- read_tsv(here("variables/variables_segments.tsv")) %>%
   pull(DOCULECT)
 
-# subset metadata, cognate db, and sources to only contain data on sample languages
+# subset metadata, cognate db to only contain data on sample languages
 metadata_sc <- filter(metadata, DOCULECT %in% segment_sample)
 cognates_sc <- filter(cognates, DOCULECT %in% segment_sample)
-# create vector of bibkeys used
-sources_sample <- metadata_sc %>%
-  separate_rows(SOURCE, sep = "; ") %>%
-  pull(unique(SOURCE))
-sources_sc <- filter(sources, BIBTEXKEY %in% sources_sample)
+
+# create vector of cognate IDs used in data set
+cognates_used <- cognates_sc %>%
+  distinct(COGIDS) %>%
+  pull()
+
+# subset protoforms file
+protoforms_sc <- filter(protoforms, COGIDS_BROAD %in% cognates_used)
 
 # write new files out
 write_tsv(metadata_sc, here("data/metadata.tsv"))
 write_tsv(cognates_sc, here("data/cognates.tsv"))
-df2bib(sources_sc, here("data/sources_subset.bib"))
+write_tsv(protoforms_sc, here("data/protoforms.tsv"))
